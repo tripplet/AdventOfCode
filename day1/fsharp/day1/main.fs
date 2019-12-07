ae9d8928d7d0c8d43342f728d57f6ad514004df4
Tobias Tangemann <tobias@tangemann.org>
2019-12-07 16:41:08 +0100

    Day 1 in F#

---
+++ day1/fsharp/day1/main.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+    
+let CalcFuelForMass mass = (int)(mass / 3) - 2
+
+let CalcFuelForFuel mass = 
+    let mutable fuel = 0
+    let mutable currentMass = mass
+    while currentMass > 8 do
+        currentMass <- CalcFuelForMass currentMass
+        fuel <- fuel + currentMass
+    fuel
+
+let GetFileContent filename = File.ReadAllLines filename |> Array.map int
+
+[<EntryPoint>]
+let main _ =
+    let mass = GetFileContent "input.txt"
+
+    let fuelPart1 = mass |> Array.map CalcFuelForMass |> Array.sum    
+    printfn "Part1: %A" fuelPart1
+        
+    let fuelPart2 = mass |> Array.map CalcFuelForFuel |> Array.sum    
+    printfn "Part2: %A" fuelPart2
+
+    0    
