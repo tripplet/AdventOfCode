a3cd482d4499b88564f40a2a9feb6ac86a60f8e5
Tobias Tangemann <tobias@tangemann.org>
2019-12-11 00:40:54 +0100

    Day 10 - Part 2

--- day10/day10.py
+++ day10/day10.py
@@ -9,88 +9,127 @@ import numpy as np
 import pdb
 
 
-def char_to_int(char):
-    return 0 if char == '.' else 1
-
-
 def parse(str):
+    char_to_int = lambda char: 0 if char == '.' else 1
     return np.array(list(map(lambda line: [char_to_int(char) for char in line], str.splitlines())))
 
+
+def get_map(filename):
+    with open(filename) as fp:
+        return parse(fp.read())
 
 def count_visible(asteroidmap, pos):
     count = 0
     localmap = asteroidmap.copy()
-
     localmap[pos[0], pos[1]] = -1
+    size_x = len(localmap[0])
+    size_y = len(localmap)
 
-    for ay in range(len(localmap)):
-        for ax in range(len(localmap[0])):
-            check_pos = np.array([ay, ax])
-
-            if localmap[ay,ax] == 0 or (check_pos[0] == pos[0] and check_pos[1] == pos[1]):
+    for ay in range(size_y):
+        for ax in range(size_x):
+            if localmap[ay,ax] <= 0:
                 continue
 
+            # possible asteroid detected
+            # calculate vector
             vector = np.array([ay, ax]) - pos
             div = math.gcd(vector[0], vector[1])
             base_vector = vector // div
-
-            if div == 1:
+            
+            if div == 1 and localmap[ay,ax] == 1:
+                # in direct line of sight
                 count += 1
                 localmap[ay,ax] = 2
-                #print(localmap)
             else:
-                if base_vector[0] != 0:
-                    nb_segements = vector[0] // base_vector[0]
-                else:
-                    nb_segements = vector[1] // base_vector[1]
-
-                found = False
-                for mult in range(1, nb_segements):
-                    possible = pos + (mult*base_vector)
-                    value = localmap[possible[0], possible[1]]
-                    if value >= 1:
-                        localmap[possible[0], possible[1]] = 3
-                        #print(localmap)
-                        found = True
-                if not found:
+                # might be obstructed
+                check = pos + base_vector                
+                
+                new = None
+                count_new = True
+                while check[0] >= 0 and check[0] < size_y and \
+                      check[1] >= 0 and check[1] < size_x:
+                    value = localmap[check[0], check[1]]
+                    if new is not None:                        
+                        if value == 2:
+                            localmap[check[0], check[1]] = 1
+                            count_new = False                     
+                    else:
+                        if value == 1:
+                            new = check
+                            localmap[check[0], check[1]] = 2
+                        elif value == 2:
+                            break                    
+                    check += base_vector
+                if count_new and new is not None:
                     count += 1
-                    localmap[ay,ax] = 4
-                    #print(localmap)
-    #print(localmap)
     return count, localmap
+
+def pos_to_angle(y, x):
+    '''map x,y pos to angle where 0 rad is up going clockwise'''
+    return ((math.pi/2 - math.atan2(-y, x)) + 2*math.pi) % (2*math.pi)
 
 def check_map(asteroidmap):
     max_count = 0
-
     result = asteroidmap.copy()
-
 
     for y in range(len(asteroidmap)):
         for x in range(len(asteroidmap[0])):
             if asteroidmap[y,x] == 0:
                 continue
-            count, location_map = count_visible(asteroidmap, np.array([y, x]))
-            #print(x, y, count)
+            count, direct_sight_map = count_visible(asteroidmap, np.array([y, x]))
             result[y, x] = count
             if count > max_count:
                 max_count = count
-                max_location_map = location_map
+                max_sight_map = direct_sight_map
                 best = (x, y)
-
-    print()
-    #print(result)
-    print(max_count, best)
-    print(max_location_map)
-    return max_location_map, (y, x)
-
-with open('input.txt') as fp:
-    asteroidmap = parse(fp.read())
+    return max_sight_map, max_count, best
 
 
-destroy_map = None
+def find_plant_destroy(asteroidmap, target):
+    max_sight_map, in_sight_count, laser_pos = check_map(asteroidmap)
+    
+    angles = []
+    lx = laser_pos[0]
+    ly = laser_pos[1]
+    loop = 1
+    
+    while True:
+        for y in range(len(max_sight_map)):
+            for x in range(len(max_sight_map[0])):
+                if max_sight_map[y, x] == 2:
+                    angles.append((pos_to_angle(y - ly, x - lx) + 2*math.pi*loop, x, y))
+                    max_sight_map[y, x] = 0 # destory asteroid        
+        angles.sort(key = lambda pos: pos[0])
+        
+        if len(angles) < target:
+            max_sight_map, _, _ = count_visible(asteroidmap, np.array([ly, lx]))
+            loop += 1
+        else:
+            break
+    
+    print(angles[target - 1], angles[target - 1][1]* 100 + angles[target - 1][2])
+        
+    
 
-try:
-    destroy_map, _ = check_map(asteroidmap)
-except:
-    pdb.post_mortem()
 
+def test(filename, pos, count):    
+    asteroidmap = get_map(filename)
+    try:
+        _, max_count, result_pos = check_map(asteroidmap)
+        if max_count != count or pos != result_pos:
+            raise Exception("Invalid result")               
+    except:
+        pdb.post_mortem()
+        
+if False:
+    # checks
+    test('test0.txt', (3, 4), 8)
+    test('test1.txt', (5, 8), 33)
+    test('test2.txt', (1, 2), 35)
+    test('test3.txt', (6, 3), 41)
+    test('test4.txt', (11, 13), 210)    
+    test('input.txt', (23, 19), 278)
+    print('Checks passed')
+
+# Part 2
+find_plant_destroy(get_map('input.txt'), 200)
