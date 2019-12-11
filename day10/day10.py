40bd59269eea46caaca5655995e03eeefc17e679
Tobias Tangemann <tobias@tangemann.org>
2019-12-11 15:33:36 +0100

    Cleanup Day 9 & 10

--- day10/day10.py
+++ day10/day10.py
@@ -35,30 +35,30 @@ def count_visible(asteroidmap, pos):
             vector = np.array([ay, ax]) - pos
             div = math.gcd(vector[0], vector[1])
             base_vector = vector // div
-            
+
             if div == 1 and localmap[ay,ax] == 1:
                 # in direct line of sight
                 count += 1
                 localmap[ay,ax] = 2
             else:
                 # might be obstructed
-                check = pos + base_vector                
-                
+                check = pos + base_vector
+
                 new = None
                 count_new = True
                 while check[0] >= 0 and check[0] < size_y and \
                       check[1] >= 0 and check[1] < size_x:
                     value = localmap[check[0], check[1]]
-                    if new is not None:                        
+                    if new is not None:
                         if value == 2:
                             localmap[check[0], check[1]] = 1
-                            count_new = False                     
+                            count_new = False
                     else:
                         if value == 1:
                             new = check
                             localmap[check[0], check[1]] = 2
                         elif value == 2:
-                            break                    
+                            break
                     check += base_vector
                 if count_new and new is not None:
                     count += 1
@@ -87,47 +87,47 @@ def check_map(asteroidmap):
 
 def find_plant_destroy(asteroidmap, target):
     max_sight_map, in_sight_count, laser_pos = check_map(asteroidmap)
-    
+
     angles = []
     lx = laser_pos[0]
     ly = laser_pos[1]
     loop = 1
-    
+
     while True:
         for y in range(len(max_sight_map)):
             for x in range(len(max_sight_map[0])):
                 if max_sight_map[y, x] == 2:
                     angles.append((pos_to_angle(y - ly, x - lx) + 2*math.pi*loop, x, y))
-                    max_sight_map[y, x] = 0 # destory asteroid        
+                    max_sight_map[y, x] = 0 # destory asteroid
         angles.sort(key = lambda pos: pos[0])
-        
+
         if len(angles) < target:
             max_sight_map, _, _ = count_visible(asteroidmap, np.array([ly, lx]))
             loop += 1
         else:
             break
-    
+
     print(angles[target - 1], angles[target - 1][1]* 100 + angles[target - 1][2])
-        
-    
 
 
-def test(filename, pos, count):    
+
+
+def test(filename, pos, count):
     asteroidmap = get_map(filename)
     try:
         _, max_count, result_pos = check_map(asteroidmap)
         if max_count != count or pos != result_pos:
-            raise Exception("Invalid result")               
+            raise Exception("Invalid result")
     except:
         pdb.post_mortem()
-        
+
 if False:
     # checks
     test('test0.txt', (3, 4), 8)
     test('test1.txt', (5, 8), 33)
     test('test2.txt', (1, 2), 35)
     test('test3.txt', (6, 3), 41)
-    test('test4.txt', (11, 13), 210)    
+    test('test4.txt', (11, 13), 210)
     test('input.txt', (23, 19), 278)
     print('Checks passed')
 
--- day9/day9.py
+++ day9/day9.py
@@ -5,17 +5,17 @@ Created on Mon Dec  9 21:21:07 2019
 @author: ttobias
 """
 
-class IntCodeProgram:
-    # syntax =>  opcode: (number of parameter, output parameter pos) 
+class IntComputer:
+    # syntax =>  opcode: (number of parameter, output parameter pos)
     opcode_param_count = {
-      1:  (3, 2), # add
-      2:  (3, 2), # multiply
-      3:  (1, 0), # input
+      1:  (3,  2), # add
+      2:  (3,  2), # multiply
+      3:  (1,  0), # input
       4:  (1, -1), # output
       5:  (2, -1), # jump-if-true
       6:  (2, -1), # jump-if-false
-      7:  (3, 2), # less-than
-      8:  (3, 2), # equals
+      7:  (3,  2), # less-than
+      8:  (3,  2), # equals
       9:  (1, -1), # adjusts-relative-base
       99: (0, -1), # end
     }
@@ -26,150 +26,128 @@ class IntCodeProgram:
         self.relative_base = 0
         self.outputs = []
 
+    @staticmethod
+    def from_string(str): return IntComputer(IntComputer.read(str))
 
+    @staticmethod
+    def from_file(filename):
+        with open(filename) as fp:
+            return IntComputer.from_string(fp.read())
 
-def read(str):
-    return list(map(lambda x : int(x), str.split(',')))
+    @staticmethod
+    def read(str): return list(map(lambda x : int(x), str.split(',')))
 
+    def _ensure_size(self, index):
+        if index >= len(self.memory):
+            self.memory = self.memory + ([0] * (index - len(self.memory) + 1))
 
-def get_value(state, index, mode, is_output):
-    if is_output:
-        if mode == 0:
-            return state.memory[index]
-        elif mode == 2:
-            return state.relative_base + state.memory[index] 
+    def __getitem__(self, index):
+        self._ensure_size(index)
+        return self.memory[index]
+
+    def __setitem__(self, index, value):
+        self._ensure_size(index)
+        self.memory[index] = value
+
+    def get(self, index, mode, is_output):
+        if is_output:
+            if mode == 0:
+                return self.memory[index]
+            elif mode == 2:
+                return self.relative_base + self.memory[index]
+            else:
+                raise Exception(f'unsupported mode {mode}')
         else:
-            raise Exception(f'unsupported mode {mode}')   
-    else:
-        if mode == 0:
-            index = state.memory[index]
-        elif mode == 1:
-            pass # index is already correct
-        elif mode == 2:
-            index = state.relative_base + state.memory[index]
+            if mode == 0:
+                index = self.memory[index]
+            elif mode == 1:
+                pass # index is already correct
+            elif mode == 2:
+                index = self.relative_base + self.memory[index]
+            else:
+                raise Exception(f'unsupported mode {mode}')
+            return self[index]
+
+    def process_instruction(self, input_values):
+        # intcode instruction decoding
+        opcode = self[self.instruction_pos] % 100
+        instruction = self[self.instruction_pos] - opcode
+        position_updated = False
+
+        # 0 = position mode, 1 = immediate mode, 2 = relative mode
+        mode_param = [-1, -1, -1]
+        mode_param[2], instruction = divmod(instruction, 10000)
+        mode_param[1], instruction = divmod(instruction, 1000)
+        mode_param[0], instruction = divmod(instruction, 100)
+
+        # get the parameters
+        param_def = IntComputer.opcode_param_count[opcode]
+        param = [0] * param_def[0]
+
+        for idx in range(param_def[0]):
+            param[idx] = self.get(self.instruction_pos + idx + 1, mode_param[idx], idx == param_def[1])
+
+        # perform instruction
+        if opcode == 1: self[param[2]] = param[0] + param[1] # add
+        elif opcode == 2: self[param[2]] = param[0] * param[1] # multiply
+        elif opcode == 3: self[param[0]] = input_values.pop(0) # input
+        elif opcode == 4: self.outputs.append(param[0]) # output
+
+        elif opcode == 5: # jump-if-true
+            if param[0] != 0:
+                self.instruction_pos = param[1]
+                position_updated = True
+        elif opcode == 6: # jump-if-false
+            if param[0] == 0:
+                self.instruction_pos = param[1]
+                position_updated = True
+        elif opcode == 7: # less-than
+            if param[0] < param[1]:
+                self[param[2]] = 1
+            else:
+                self[param[2]] = 0
+        # equals
+        elif opcode == 8:
+            if param[0] == param[1]:
+                self[param[2]] = 1
+            else:
+                self[param[2]] = 0
+
+        elif opcode == 9: self.relative_base += param[0] # adjusts-relative-base
+        elif opcode == 99: return False # end
         else:
-            raise Exception(f'unsupported mode {mode}')
-    
-        if index >= len(state.memory):
-            state.memory = state.memory + ([0] * (index - len(state.memory) + 1))
-    
-        return state.memory[index]
+            raise Exception(f'unsupported opcode {opcode}')
+
+        if not position_updated: self.instruction_pos += param_def[0] + 1
+        return True
+
+    def execute(self, input_values):
+        run = True
+        while run: run = self.process_instruction(input_values)
 
 
-def set_value(state, index, value):
-    if index >= len(state.memory):
-        state.memory = state.memory + ([0] * (index - len(state.memory) + 1))
-
-    state.memory[index] = value
+test_prog1 = IntComputer.from_string('109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99')
+test_prog2 = IntComputer.from_string('1102,34915192,34915192,7,4,7,99,0')
+test_prog3 = IntComputer.from_string('104,1125899906842624,99')
 
 
-
-def process_instruction(state, input_values):
-    # intcode instruction decoding
-    opcode = state.memory[state.instruction_pos] % 100
-    instruction = state.memory[state.instruction_pos] - opcode
-    position_updated = False
-
-    # 0 = position mode, 1 = immediate mode, 2 = relative mode
-    mode_param = [-1, -1, -1]
-    mode_param[2], instruction = divmod(instruction, 10000)
-    mode_param[1], instruction = divmod(instruction, 1000)
-    mode_param[0], instruction = divmod(instruction, 100)
-
-    # get the parameters
-    param_def = IntCodeProgram.opcode_param_count[opcode]
-    param = [0] * param_def[0]
-
-    for idx in range(param_def[0]):
-        param[idx] = get_value(state, state.instruction_pos + idx + 1, mode_param[idx], idx == param_def[1])
-
-    # perform instruction
-    # add
-    if opcode == 1:
-        set_value(state, param[2], param[0] + param[1])
-
-    # multiply
-    elif opcode == 2:
-        set_value(state, param[2], param[0] * param[1])
-
-    # input
-    elif opcode == 3:
-        set_value(state, param[0], input_values.pop(0))
-
-    # output
-    elif opcode == 4:
-        state.outputs.append(param[0])
-
-    # jump-if-true
-    elif opcode == 5:
-        if param[0] != 0:
-            state.instruction_pos = param[1]
-            position_updated = True
-
-    # jump-if-false
-    elif opcode == 6:
-        if param[0] == 0:
-            state.instruction_pos = param[1]
-            position_updated = True
-
-    # less-than
-    elif opcode == 7:
-        if param[0] < param[1]:
-            set_value(state, param[2], 1)
-        else:
-            set_value(state, param[2], 0)
-
-    # equals
-    elif opcode == 8:
-        if param[0] == param[1]:
-            set_value(state, param[2], 1)
-        else:
-            set_value(state, param[2], 0)
-
-    # adjusts-relative-base
-    elif opcode == 9:
-        state.relative_base += param[0]
-
-    # end
-    elif opcode == 99:
-        return False
-
-    else:
-        raise Exception(f'unsupported opcode {opcode}')
-
-    if not position_updated:
-        state.instruction_pos += param_def[0] + 1
-    return True
-
-
-def execute(computer_state, input_values):
-    run = True
-    while run:
-        run = process_instruction(computer_state, input_values)
-
-
-with open('input.txt') as fp:
-    data = read(fp.read())
-
-
-test_prog1 = IntCodeProgram(read('109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99'))
-test_prog2 = IntCodeProgram(read('1102,34915192,34915192,7,4,7,99,0'))
-test_prog3 = IntCodeProgram(read('104,1125899906842624,99'))
-
-
-execute(test_prog1, [])
+test_prog1.execute([])
 print(f'Test 1: {test_prog1.outputs}')
 
-execute(test_prog2, [])
+test_prog2.execute([])
+assert test_prog2.outputs[0] == 1219070632396864
 print(f'Test 2: {test_prog2.outputs}')
 
-execute(test_prog3, [])
+test_prog3.execute([])
+assert test_prog3.outputs[0] == 1125899906842624
 print(f'Test 3: {test_prog3.outputs}')
 
-boost_prog_part1 = IntCodeProgram(data)
-execute(boost_prog_part1, [1])
+boost_prog_part1 = IntComputer.from_file('input.txt')
+boost_prog_part1.execute([1])
+assert boost_prog_part1.outputs[0] == 2752191671
 print(f'Part 1: {boost_prog_part1.outputs}')
 
-boost_prog_part2 = IntCodeProgram(data)
-execute(boost_prog_part2, [2])
+boost_prog_part2 = IntComputer.from_file('input.txt')
+boost_prog_part2.execute([2])
+assert boost_prog_part2.outputs[0] == 87571
 print(f'Part 2: {boost_prog_part2.outputs}')
