284cb95d4bafb219e23b5a127d8907e2367806f7
Tobias Tangemann <tobias@tangemann.org>
2019-12-19 17:24:14 +0100

    Day 17 - Part 2 (Incomplete)

--- day17/day17.py
+++ day17/day17.py
@@ -8,6 +8,13 @@ from itertools import product
 
 from IntComputer import IntComputer
 
+def rotate(y, x):
+    if   y ==  1 and x ==  0: return  0, -1
+    elif y ==  0 and x == -1: return -1,  0
+    elif y == -1 and x ==  0: return  0,  1
+    elif y ==  0 and x ==  1: return  1,  0
+
+
 prog = IntComputer.from_file('input.txt')
 prog.execute([])
 
@@ -25,11 +32,42 @@ for char in prog.outputs:
 del card[len(card) - 1]
 
 scaffold = ord('#')
-crossings = 0
+part1 = 0
 for y, x in product(range(1, len(card) - 1), range(1, len(card[0]) - 1)):
     if card[y][x] == scaffold and \
        card[y+1][x] == scaffold and \
        card[y-1][x] == scaffold and \
        card[y][x+1] == scaffold and \
        card[y][x-1] == scaffold:
-          crossings += x * y
+          part1 += x * y
+
+print(f'Part1: {part1}')
+
+
+# Find robot
+for y, x in product(range(1, len(card) - 1), range(1, len(card[0]) - 1)):
+    v = chr(card[y][x])
+    if v == '^' or v == '<' or v == '>' or v == 'v':
+        robot = (y, x)
+
+# Find path
+path = []
+pos_x = robot[1]
+pos_y = robot[0]
+dir_x = 1
+dir_y = 0
+r = 0
+while True:
+    next_pos_x = pos_x + dir_x
+    next_pos_y = pos_y + dir_y
+    if card[next_pos_y][next_pos_x] == scaffold:
+        path[len(path) - 1] +=1
+        pos_x = next_pos_x
+        pos_y = next_pos_y
+        r = 0
+    else:
+        dir_y, dir_x = rotate_left(dir_y, dir_x)
+        r += 1
+        if r
+        path.append('L')
+        path.append(0)
