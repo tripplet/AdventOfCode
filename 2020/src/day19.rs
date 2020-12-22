a7b100131d748ffa7294f027e210b3110e0a47a1
Tobias Tangemann <tobias@tangemann.org>
2020-12-22 12:14:36 +0100

    Progress - Day 19 part2

--- 2020/src/day19.rs
+++ 2020/src/day19.rs
@@ -7,9 +7,14 @@ fn main() {
     let data_example = parse(include_str!("../input/2020/day19.txt")).unwrap();
 
     let now = std::time::Instant::now();
-    part1(&data_example);
-    //println!("Part1: {}  [{}]", part1, humantime::format_duration(now.elapsed()));
-    //assert_eq!(part2, 825305207525452);
+    let part1_result = part1(&data_example);
+    println!("Part1: {}  [{}]", part1_result, humantime::format_duration(now.elapsed()));
+    assert_eq!(part1_result, 156);
+
+    let now = std::time::Instant::now();
+    let part2 = part2(&parse(include_str!("../input/2020/day19.txt")).unwrap());
+    println!("Part2: {}  [{}]", part2, humantime::format_duration(now.elapsed()));
+    assert_eq!(part2, 363);
 }
 
 #[derive(Debug)]
@@ -18,7 +23,7 @@ struct InputData {
     messages: Vec<Vec<char>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Rule {
     Character(char),
     Ref(u64),
@@ -73,56 +78,83 @@ fn parse(input: &str) -> Result<InputData, Box<dyn Error>> {
     Ok(InputData{rules: rules, messages: messages})
 }
 
-fn part1(data: &InputData) {
+fn part1(data: &InputData) -> usize {
     //dbg!(data);
 
     let results = data.messages.iter().map(|msg|
-        check_message(data, data.rules.get(&0).unwrap(),msg, 0).and_then(|res| Some(res == msg.len())).unwrap_or(false)
+        check_message(data, data.rules.get(&0).unwrap(),msg, 0).iter().any(|r| *r == msg.len())
     ).collect::<Vec<_>>();
 
-    dbg!(&results);
-    dbg!(&results.iter().filter(|r| **r).count());
+    //dbg!(&results);
+    results.iter().filter(|r| **r).count()
     //check_message(0, data.messages[0], backtrack_stack)
 }
 
-fn check_message(data: &InputData, rule_to_check: &Rule, msg: &Vec<char>, pos: usize) -> Option<usize> {
-    let mut result = None;
+fn part2(orig_data: &InputData) -> usize {
+    let mut patched_rules = orig_data.rules.clone();
+    patched_rules.insert(8, Rule::from_str("42 | 42 8").unwrap());
+    patched_rules.insert(11, Rule::from_str("42 31 | 42 11 31").unwrap());
+
+    let data = InputData{rules: patched_rules, messages: orig_data.messages.clone()};
+    //let data = InputData{rules: patched_rules, messages: vec![orig_data.messages[2].clone()]};
+    //let data = InputData{rules: patched_rules, messages: vec!["bbbbbbbaaaabbbbaaabbabaaa".chars().collect()]};
+
+    let results = data.messages.iter().map(|msg|
+        (msg.len(), check_message(&data, data.rules.get(&0).unwrap(),msg, 0))
+    ).collect::<Vec<_>>();
+
+    //dbg!(&results);
+
+    let rr = results.iter().map(|results| results.1.iter().any(|res| *res == results.0)).collect::<Vec<_>>();
+
+    //dbg!(&rr);
+    //.and_then(|res| Some(format!("{} = {}",res, msg.len()))).unwrap_or("None".into())
+
+    //dbg!(&results);
+    rr.iter().filter(|r| **r).count()
+    //check_message(0, data.messages[0], backtrack_stack)
+    //0
+}
+
+fn check_message(data: &InputData, rule_to_check: &Rule, msg: &Vec<char>, pos: usize) -> Vec<usize> {
 
     //println!("pos: {}, {:?}", pos, rule_to_check);
 
+    let result = match rule_to_check {
+        Rule::Sub(rule) => {
+            let mut positions = vec![pos];
 
-    if let Rule::Sub(sub_rules) = rule_to_check {
-        let mut cur_pos = pos;
-        for sub_rule in sub_rules {
-            result = check_message(data, sub_rule, msg, cur_pos);
-            if let Some(new_pos) = result {
-                cur_pos = new_pos
+            for sub_rule in rule {
+                let mut sub_result = vec![];
+                for cur_position in positions {
+                    if cur_position < msg.len() {
+                        sub_result.extend(check_message(data, sub_rule, msg, cur_position));
+                    }
+                }
+
+                positions = sub_result;
+            }
+
+            positions
+        },
+        Rule::Or(or_left, or_right) => {
+            // try both or parts (necessary for loops) and combine them
+            let left = check_message(data, or_left, msg, pos);
+            let right = check_message(data, or_right, msg, pos);
+
+            [&left[..], &right[..]].concat()
+        },
+        Rule::Ref(ref_rule) => check_message(data, data.rules.get(&ref_rule).unwrap(), msg, pos),
+        Rule::Character(ch) => {
+            if msg[pos] == *ch {
+                vec![pos+1]
             }
             else {
-                break;
+                vec![]
             }
         }
-    }
-    else if let Rule::Or(or_left, or_right) = rule_to_check {
-        // try or parts
-        if let Some(res) = check_message(data, or_left, msg, pos) {
-            result = Some(res);
-        } else {
-            result = check_message(data, or_right, msg, pos);
-        }
-    }
-    else if let Rule::Ref(ref_rule) = rule_to_check {
-        result = check_message(data, data.rules.get(&ref_rule).unwrap(), msg, pos);
-    }
-    else if let Rule::Character(ch) = rule_to_check {
-        if msg[pos] == *ch {
-            result = Some(pos+1);
-        }
-        else {
-            result = None;
-        }
-    }
+    };
 
-    //println!("  <- {:?}", &result);
+    //println!("    {:?}  <- {:?}", rule_to_check, &result);
     result
 }
