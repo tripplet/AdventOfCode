652d2b301f145539bc053af5c314657319627953
Tobias Tangemann <tobias@tangemann.org>
2021-12-06 15:26:30 +0100

    Day 6 - Part 2 (+ cleanup part 1)

--- 2021/src/day6.rs
+++ 2021/src/day6.rs
@@ -9,41 +9,38 @@ fn parse_input(input: &str) -> Result<Vec<i8>, std::num::ParseIntError> {
     input.trim().split(",").map(|nb| nb.parse()).collect()
 }
 
-fn part1(fish: &[i8]) -> usize { run_simulation(fish, 80) }
-fn part2(fish: &[i8]) -> usize { run_simulation(fish, 256) }
+fn part1(fish: &[i8]) -> usize { calculate_generations(fish, 80) }
+fn part2(fish: &[i8]) -> usize { calculate_generations(fish, 256) }
 
-fn run_simulation(fish_timers: &[i8], days: u16) -> usize {
-    let mut fish_timers = fish_timers.clone().to_vec();
+fn calculate_generations(fish_timers: &[i8], days: u16) -> usize {
+    let fish_timers = fish_timers.clone().to_vec();
+
+    let mut fish_counter = [0; 9];
+    fish_timers
+        .iter()
+        .for_each(|&f| fish_counter[f as usize] += 1);
 
     for _ in 0..days {
-        let mut new_fish = 0;
-        for idx in 0..fish_timers.len() {
-            let fish = &mut fish_timers[idx];
-            *fish -= 1;
-
-            if *fish == -1 {
-                *fish = 6;
-                new_fish += 1;
-            }
-        }
-
-        fish_timers.append(&mut vec![8; new_fish]);
+        fish_counter.rotate_left(1);
+        fish_counter[6] += fish_counter[8];
     }
 
-    fish_timers.len()
+    fish_counter.iter().sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EXAMPLE_INPUT: &str = "3,4,3,1,2";
+
     #[test]
     fn part1_example() {
-        assert_eq!(5934, part1(&parse_input("3,4,3,1,2").unwrap()));
+        assert_eq!(5934, part1(&parse_input(EXAMPLE_INPUT).unwrap()));
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(26984457539, part2(&parse_input("3,4,3,1,2").unwrap()));
+        assert_eq!(26984457539, part2(&parse_input(EXAMPLE_INPUT).unwrap()));
     }
 }
