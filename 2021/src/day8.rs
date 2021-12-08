84c86a92ce6f7bf1f42ddbe212815a8b632c3b7b
Tobias Tangemann <tobias@tangemann.org>
2021-12-08 23:58:12 +0100

    Cleanup day 8

--- 2021/src/day8.rs
+++ 2021/src/day8.rs
@@ -1,8 +1,6 @@
 use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
-use std::io::prelude::*;
-use std::io::{self, Write};
 
 const INPUT: &str = include_str!("../input/2021/day8.txt");
 
@@ -22,11 +20,10 @@ fn parse_input(input: &str) -> Data {
         .lines()
         .map(|line| {
             line.split("|")
-                .map(|digits| {
-                    digits
-                        .trim()
+                .map(|sym| {
+                    sym.trim()
                         .split_whitespace()
-                        .map(|digits| digits.chars().collect::<HashSet<_>>())
+                        .map(|sym| sym.chars().collect::<HashSet<_>>())
                         .collect::<Vec<_>>()
                 })
                 .collect::<Vec<_>>()
@@ -38,8 +35,8 @@ fn part1(data: &Data) -> usize {
     data.iter().fold(0, |sum, line| {
         sum + line[1]
             .iter()
-            .map(|digits| {
-                let len = digits.len();
+            .map(|sym| {
+                let len = sym.len();
                 if len == 2 || len == 4 || len == 3 || len == 7 {
                     1
                 } else {
@@ -51,119 +48,70 @@ fn part1(data: &Data) -> usize {
 }
 
 fn part2(data: &Data) -> Result<usize, Box<dyn Error>> {
+    //  aaaa
+    // b    c
+    // b    c
+    //  dddd
+    // e    f
+    // e    f
+    //  gggg
     let mut sum = 0;
     for line in data {
-        //  aaaa
-        // b    c
-        // b    c
-        //  dddd
-        // e    f
-        // e    f
-        //  gggg
-
+        let scrambled_digits = &line[0];
+        let mut dig: Vec<Option<&HashSet<char>>> = vec![None; 10];
         let mut decryption: HashMap<String, u8> = HashMap::new();
 
-        let d_1 = line[0]
-            .iter()
-            .find(|digits| digits.len() == 2)
-            .ok_or("No 1 found")?;
-        let d_4 = line[0]
-            .iter()
-            .find(|digits| digits.len() == 4)
-            .ok_or("No 4 found")?;
-        let d_7 = line[0]
-            .iter()
-            .find(|digits| digits.len() == 3)
-            .ok_or("No 7 found")?;
-        let d_8 = line[0]
-            .iter()
-            .find(|digits| digits.len() == 7)
-            .ok_or("No 8 found")?;
+        dig[1] = Some(scrambled_digits.iter().find(|sym| sym.len() == 2).ok_or("1 not found")?,);
+        dig[4] = Some(scrambled_digits.iter().find(|sym| sym.len() == 4).ok_or("4 not found")?,);
+        dig[7] = Some(scrambled_digits.iter().find(|sym| sym.len() == 3).ok_or("7 not found")?,);
+        dig[8] = Some(scrambled_digits.iter().find(|sym| sym.len() == 7).ok_or("8 not found")?,);
 
-        let d_235 = line[0]
-            .iter()
-            .filter(|digits| digits.len() == 5)
-            .collect::<Vec<_>>();
-        let d_069 = line[0]
-            .iter()
-            .filter(|digits| digits.len() == 6)
-            .collect::<Vec<_>>();
-        //dbg!(&d_235);
-        //dbg!(&d_069);
+        let digits_235: Vec<_> = scrambled_digits.iter().filter(|sym| sym.len() == 5).collect();
+        let digits_069: Vec<_> = scrambled_digits.iter().filter(|sym| sym.len() == 6).collect();
 
-        let lines_cf: HashSet<_> = d_1.intersection(&d_7).cloned().collect();
-        //dbg!(&lines_cf);
+        dig[6] = Some(digits_069.iter()
+                .find(|&d| dig[1].unwrap().intersection(d).count() != 2)
+                .copied().ok_or("6 not found")?);
 
-        let d_3 = d_235
-            .iter()
-            .find(|&d| lines_cf.intersection(d).count() == 2)
-            .ok_or("No 3 found")?;
-        let d_6 = d_069
-            .iter()
-            .find(|&d| lines_cf.intersection(d).count() != 2)
-            .ok_or("No 6 found")?;
-        let d_9 = d_069
-            .iter()
-            .find(|&d| d_4.intersection(d).count() == 4)
-            .ok_or("No 9 found")?;
-        let d_0 = d_069
-            .iter()
-            .find(|&d| d != d_6 && d != d_9)
-            .ok_or("No 0 found")?;
-        let d_2 = d_235
-            .iter()
-            .filter(|&d| d != d_3)
-            .find(|d_25| d_25.difference(&d_9).count() == 1)
-            .ok_or("No 2 found")?;
-        let d_5 = d_235
-            .iter()
-            .find(|&d| d != d_3 && d != d_2)
-            .ok_or("No 5 found")?;
+        dig[9] = Some(digits_069.iter()
+                .find(|&d| dig[4].unwrap().intersection(d).count() == 4)
+                .copied().ok_or("9 not found")?,);
 
-        // dbg!(&d_1);
-        // dbg!(&d_2);
-        // dbg!(&d_3);
-        // dbg!(&d_4);
-        // dbg!(&d_5);
-        // dbg!(&d_6);
-        // dbg!(&d_7);
-        // dbg!(&d_8);
-        // dbg!(&d_9);
-        // dbg!(&d_0);
+        dig[0] = Some(digits_069.iter()
+                .find(|&&d| d != dig[6].unwrap() && d != dig[9].unwrap())
+                .copied().ok_or("0 not found")?);
 
-        decryption.insert(d_0.iter().sorted().collect::<String>(), 0);
-        decryption.insert(d_1.iter().sorted().collect::<String>(), 1);
-        decryption.insert(d_2.iter().sorted().collect::<String>(), 2);
-        decryption.insert(d_3.iter().sorted().collect::<String>(), 3);
-        decryption.insert(d_4.iter().sorted().collect::<String>(), 4);
-        decryption.insert(d_5.iter().sorted().collect::<String>(), 5);
-        decryption.insert(d_6.iter().sorted().collect::<String>(), 6);
-        decryption.insert(d_7.iter().sorted().collect::<String>(), 7);
-        decryption.insert(d_8.iter().sorted().collect::<String>(), 8);
-        decryption.insert(d_9.iter().sorted().collect::<String>(), 9);
+        dig[3] = Some(digits_235.iter()
+                .find(|&d| d.difference(dig[1].unwrap()).count() == 3)
+                .copied().ok_or("3 not found")?);
 
-        //dbg!(&decryption);
+        dig[2] = Some(digits_235.iter()
+                .filter(|&&d| d != dig[3].unwrap())
+                .find(|d_25| d_25.difference(dig[9].unwrap()).count() == 1)
+                .copied().ok_or("2 not found")?);
 
-        // now finaly decrypt
+        dig[5] = Some(digits_235.iter()
+                .find(|&&d| d != dig[3].unwrap() && d != dig[2].unwrap())
+                .copied().ok_or("5 not found")?);
+
+        // Build decryption map
+        for (idx, symbols) in dig.iter().enumerate() {
+            decryption.insert(symbols.unwrap().iter().sorted().collect::<String>(), idx as u8);
+        }
+
+        // now finally decrypt
         let decoded_number = line[1]
             .iter()
-            .map(|enc| {
+            .map(|enc|
                 decryption
                     .get(&enc.iter().sorted().collect::<String>())
-                    .ok_or("invalid key")
-            })
-            .collect::<Result<Vec<_>, _>>()?.iter().rev().enumerate().fold(0, |acc, v|
-            acc + **v.1 as usize * (10_usize).pow(v.0 as u32)
-        );
-
-        dbg!(decoded_number);
+                    .ok_or("invalid key"))
+            .collect::<Result<Vec<_>, _>>()?
+            .iter().rev().enumerate()
+            .fold(0, |acc, v| acc + **v.1 as usize * (10_usize).pow(v.0 as u32));
 
         sum += decoded_number;
-
-        //io::stdout().flush().unwrap();
-        //let _ = io::stdin().read(&mut [0u8]).unwrap();
     }
-
     Ok(sum)
 }
 
@@ -171,6 +119,7 @@ fn part2(data: &Data) -> Result<usize, Box<dyn Error>> {
 mod tests {
     use super::*;
 
+    const EXAMPLE_SINGLE: &str = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
     const EXAMPLE: &str = include_str!("../input/2021/day8_example.txt");
 
     #[test]
@@ -179,22 +128,22 @@ mod tests {
     }
 
     #[test]
+    fn part2_example_single_line() {
+        assert_eq!(5353, part2(&parse_input(EXAMPLE_SINGLE)).unwrap());
+    }
+
+    #[test]
     fn part2_example() {
         assert_eq!(61229, part2(&parse_input(EXAMPLE)).unwrap());
     }
 
     #[test]
-    fn part2_example2() {
-        assert_eq!(5353, part2(&parse_input("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")).unwrap());
+    fn part1_on_input() {
+        assert_eq!(362, part1(&parse_input(INPUT)));
     }
 
-    //#[test]
-    // fn part1_on_input() {
-    //     //assert_eq!(, part1(&parse_input(INPUT)));
-    // }
-
-    //#[test]
-    // fn part2_on_input() {
-    //     //assert_eq!(, part2(&parse_input(INPUT)));
-    // }
+    #[test]
+    fn part2_on_input() {
+        assert_eq!(1020159, part2(&parse_input(INPUT)).unwrap());
+    }
 }
