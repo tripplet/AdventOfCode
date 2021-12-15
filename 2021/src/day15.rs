0abbbf22d9c934723dc825f35d773fb560be20ba
Tobias Tangemann <tobias@tangemann.org>
2021-12-16 00:57:56 +0100

    Day 15 - Part 2

--- 2021/src/day15.rs
+++ 2021/src/day15.rs
@@ -1,5 +1,7 @@
 use ndarray::{s, Array, Array2, Ix2};
+use itertools::{iproduct, Itertools};
 use std::iter::Iterator;
+use std::io::{self, Write};
 
 const INPUT: &str = include_str!("../input/2021/day15.txt");
 const EXAMPLE: &str = include_str!("../input/2021/day15_example.txt");
@@ -40,7 +42,7 @@ fn replicate_matrix(repeat: &[usize], data: &Array2<u8>) -> Array2<u8> {
             let mut cc = data + x as u8 + y as u8;
 
             for elem in cc.iter_mut() {
-                if (*elem > 9) {
+                if *elem > 9 {
                     *elem = *elem - 9;
                 }
             }
@@ -61,12 +63,6 @@ fn get_adjescent(shape: &[usize], point: (usize, usize)) -> Box<impl Iterator<It
         if y as isize + 1 < shape[0] as isize {Some((y+1, point.1))} else { None },
         if x as isize - 1 >= 0 {Some((y, x-1))} else { None },
         if x as isize + 1 < shape[1] as isize {Some((y, x+1))} else { None },
-
-        // No diagonal
-        // if y as isize - 1 >= 0 && x as isize - 1 >=0 {Some((y-1, x-1))} else { None },
-        // if y as isize - 1 >= 0 && x as isize + 1 < shape[1] as isize {Some((y-1, x+1))} else { None },
-        // if y as isize + 1 < shape[0] as isize && x as isize + 1 < shape[1] as isize {Some((y+1, x+1))} else { None },
-        // if y as isize + 1 < shape[0] as isize && x as isize - 1 >=0 as isize {Some((y+1, x-1))} else { None },
     ];
 
     Box::new(adjescent.into_iter().filter_map(std::convert::identity))
@@ -75,25 +71,33 @@ fn get_adjescent(shape: &[usize], point: (usize, usize)) -> Box<impl Iterator<It
 fn part2(data: &Data) -> usize {
     let copied_matrix = replicate_matrix(&[5, 5], data);
 
-    println!("{}", copied_matrix);
+    //println!("{}", copied_matrix);
     part1(&copied_matrix)
 }
 
 fn part1(data: &Data) -> usize {
-    let mut data = data;
-    let mut visited = Array::from_elem(data.shape(), false).into_dimensionality::<Ix2>().unwrap();
     let mut costs = Array::from_elem(data.shape(), u32::MAX).into_dimensionality::<Ix2>().unwrap();
 
     costs[[0,0]] = 0;
     let mut queue = vec![(0,0)];
 
+    //let mut all = iproduct!(0..data.shape()[1], 0..data.shape()[0]).collect::<Vec<_>>();
+
     loop {
-        let current = queue.pop();
-        if current.is_none() {
+        queue.sort_unstable_by(|a, b| costs[[a.0, a.1]].partial_cmp(&costs[[b.0, b.1]]).unwrap());
+
+        //let sorted_values = queue.iter().map(|&(y, x)| costs[[y, x]]).collect::<Vec<_>>();
+
+        let current = if queue.len() > 0 {
+            queue.remove(0)
+        } else {
+            break;
+        };
+
+        if current == (data.shape()[0] - 1, data.shape()[1] - 1) {
             break;
         }
 
-        let current = current.unwrap();
         let adjescent = get_adjescent(data.shape(), current).collect::<Vec<_>>();
 
         let cur_v = costs[[current.0, current.1]];
@@ -109,7 +113,7 @@ fn part1(data: &Data) -> usize {
         }
     }
 
-    println!("{}", costs);
+    //println!("{}", costs);
 
     costs[[data.shape()[0] - 1, data.shape()[1] - 1]] as usize
 }
