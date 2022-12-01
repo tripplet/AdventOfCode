5f551089dee930942fcc9092c74ec9adbc790916
Tobias Tangemann <tobias@tangemann.org>
2022-12-01 18:02:19 +0100

    Add template

---
+++ 2022/src/template.rs
@@ -0,0 +1,14 @@
+type NUMBER = i32;
+type ParseResult = Vec<NUMBER>;
+
+pub fn parse_input(input: &str) -> ParseResult {
+    vec![]
+}
+
+pub fn part1(input: &ParseResult) -> isize {
+    42
+}
+
+pub fn part2(input: &ParseResult) -> isize {
+    42
+}
