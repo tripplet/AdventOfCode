dbbef146e2919798206d48b872493cdc79e11524
Tobias Tangemann <tobias@tangemann.org>
2022-12-21 01:02:31 +0100

    Improve template

--- 2022/src/template.rs
+++ 2022/src/template.rs
@@ -12,3 +12,35 @@ pub fn part1(input: &ParseResult) -> isize {
 pub fn part2(input: &ParseResult) -> isize {
     42
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../input/2022/day_example.txt");
+    const INPUT: &str = include_str!("../input/2022/day.txt");
+
+    #[test]
+    fn example_part1() {
+        let input = parse_input(EXAMPLE);
+        assert_eq!(part1(&input), 42);
+    }
+
+    #[test]
+    fn example_part2() {
+        let input = parse_input(EXAMPLE);
+        assert_eq!(part2(&input), 42);
+    }
+
+    #[test]
+    fn input_part1() {
+        let input = parse_input(INPUT);
+        assert_eq!(part1(&input), 42);
+    }
+
+    #[test]
+    fn input_part2() {
+        let input = parse_input(INPUT);
+        assert_eq!(part2(&input), 42);
+    }
+}
