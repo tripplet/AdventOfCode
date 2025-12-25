fac7fc9c622d0a11fda74377e8b5788d67f802f0
Tobias Tangemann <tobias@tangemann.org>
2025-12-25 20:12:56 +0100

    Day 9 - Part 2

--- 2025/src/day09.rs
+++ 2025/src/day09.rs
@@ -1,132 +1,167 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
-type Number = u64;
+type Number = usize;
+type Vec2 = crate::utils::Vec2<Number>;
 type ParseResult = Vec<Vec2>;
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Vec2 {
-    x: Number,
-    y: Number,
-}
 
 #[aoc_generator(day9)]
 pub fn parse_input(input: &str) -> ParseResult {
     input
         .trim()
         .lines()
-        .map(|line| line.parse::<Vec2>().unwrap())
+        .map(|line| {
+            let mut parts = line
+                .split(',')
+                .map(|nb| nb.trim().parse::<Number>().unwrap());
+            Vec2 {
+                x: parts.next().unwrap(),
+                y: parts.next().unwrap(),
+            }
+        })
         .collect()
 }
 
-impl std::str::FromStr for Vec2 {
-    type Err = &'static str;
+// All line segments are either horizontal or vertical
+#[rustfmt::skip]
+#[derive(Debug)]
+enum Edge {
+    Vertical   { x: Number, y_min: Number, y_max: Number },
+    Horizontal { y: Number, x_min: Number, x_max: Number },
+}
 
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<Number> = input
-            .split(',')
-            .map(|nb| nb.trim().parse::<Number>())
-            .collect::<Result<Vec<_>, _>>()
-            .map_err(|_| "Failed to parse integer")?;
-
-        if parts.len() != 2 {
-            return Err("Expected three comma-separated numbers");
+impl From<(Vec2, Vec2)> for Edge {
+    #[rustfmt::skip]
+    fn from((p1, p2): (Vec2, Vec2)) -> Self {
+        if p1.y == p2.y {
+            Edge::Horizontal { y: p1.y, x_min: p1.x.min(p2.x), x_max: p1.x.max(p2.x) }
+        } else {
+            Edge::Vertical { x: p1.x, y_min: p1.y.min(p2.y), y_max: p1.y.max(p2.y) }
         }
-
-        Ok(Vec2 {
-            x: parts[0],
-            y: parts[1],
-        })
     }
 }
 
-impl Vec2 {
-    fn size(&self, other: &Vec2) -> Number {
-        (self.x.abs_diff(other.x) + 1) * (self.y.abs_diff(other.y) + 1)
+#[derive(Debug, Copy, Clone)]
+struct Rect {
+    p1: Vec2,
+    p2: Vec2,
+}
+
+impl std::fmt::Display for Rect {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Rect [{{ y:{}, x:{} }}, {{ y:{}, x:{} }}]",
+            self.p1.y, self.p1.x, self.p2.y, self.p2.x
+        )
+    }
+}
+
+impl Rect {
+    fn size(&self) -> Number {
+        (self.p1.x.abs_diff(self.p2.x) + 1) * (self.p1.y.abs_diff(self.p2.y) + 1)
     }
 }
 
 #[aoc(day9, part1)]
-pub fn part1(input: &ParseResult) -> Number {
-    let mut larges_size = 0;
-
-    for idx in 0..input.len() {
-        for idy in 0..input.len() {
-            let size = input[idx].size(&input[idy]);
-            larges_size = larges_size.max(size);
-        }
-    }
-
-    larges_size
+pub fn part1(input: &ParseResult) -> usize {
+    input
+        .iter()
+        .tuple_combinations()
+        .map(|(&p1, &p2)| Rect { p1, p2 }.size())
+        .max()
+        .unwrap()
 }
 
 #[aoc(day9, part2)]
-pub fn part2(input: &ParseResult) -> Number {
+pub fn part2(coordinates: &ParseResult) -> usize {
     let mut larges_size = 0;
-    let mut found = None;
+    // let mut top_size = None;
 
-    let mut lines: Vec<_> = input.windows(2).map(|win| (win[0], win[1])).collect();
-    lines.push((input[input.len() - 1], input[0]));
+    let edges = coordinates
+        .iter()
+        .chain(&[coordinates[0]]) // Append the first to the end to close polygon
+        .tuple_windows()
+        .fold(vec![], |mut edges, (p1, p2)| {
+            edges.push((*p1, *p2).into());
+            edges
+        });
 
-    for idx in 0..input.len() {
-        'outer: for idy in 0..input.len() {
-            let size = input[idx].size(&input[idy]);
-            if size <= larges_size {
-                continue;
-            }
-
-            for pos in input {
-                if is_inside(&(input[idx], input[idy]), pos) {
-                    continue 'outer;
-                }
-
-                for line in &lines {
-                    if intersects(&(input[idx], input[idy]), line) {
-                        continue 'outer;
-                    }
-                }
-            }
-
-            larges_size = larges_size.max(size);
-            found = Some((&input[idx], &input[idy]));
+    // Go over all possible rects
+    'outer: for (&p1, &p2) in coordinates.iter().tuple_combinations() {
+        let rect = Rect { p1, p2 };
+        let size = rect.size();
+        if size < larges_size {
+            continue;
         }
+
+        for polygon_edge in &edges {
+            if crosses_rect_inside(&rect, polygon_edge) {
+                // println!("Rejected {rect}: {size} because of edge:{polygon_edge:?}");
+                continue 'outer;
+            }
+        }
+
+        // Check if rect is inside the polygon via raycast from top
+        let check_pos_inside_x = p1.x.min(p2.x) + (p1.x.abs_diff(p2.x) / 2);
+        let check_pos_inside_y = p1.y.min(p2.y) + (p1.y.abs_diff(p2.y) / 2);
+
+        let ray = Edge::Vertical {
+            x: check_pos_inside_x,
+            y_min: 0,
+            y_max: check_pos_inside_y,
+        };
+
+        let mut intersection_count = 0;
+
+        for polygon_edge in &edges {
+            if intersects_edge(&ray, polygon_edge) {
+                intersection_count += 1;
+            }
+        }
+
+        if intersection_count % 2 == 0 {
+            // println!("Rejected {rect}: {size} intersection_count:{intersection_count}");
+            continue 'outer;
+        }
+
+        larges_size = size;
+        // top_size = Some((p1, p2));
+        // println!("----- Found {rect}: {size} intersection_count:{intersection_count}");
     }
 
-    dbg!(found);
     larges_size
 }
 
-fn is_inside(pair: &(Vec2, Vec2), point: &Vec2) -> bool {
-    (pair.0.x.min(pair.1.x) + 1..(pair.0.x.max(pair.1.x) - 1)).contains(&point.x)
-        && (pair.0.y.min(pair.1.y) + 1..(pair.0.y.max(pair.1.y) - 1)).contains(&point.y)
+fn intersects_edge(line1: &Edge, line2: &Edge) -> bool {
+    match (line1, line2) {
+        (Edge::Vertical { x, y_min, y_max }, Edge::Horizontal { y, x_min, x_max })
+        | (Edge::Horizontal { y, x_min, x_max }, Edge::Vertical { x, y_min, y_max }) => {
+            (x_min <= x && x <= x_max) && (y_min <= y && y <= y_max)
+        }
+        (Edge::Horizontal { .. }, Edge::Horizontal { .. })
+        | (Edge::Vertical { .. }, Edge::Vertical { .. }) => false,
+    }
 }
 
-fn intersects(rect_diagonale: &(Vec2, Vec2), line2: &(Vec2, Vec2)) -> bool {
-    let x1 = rect_diagonale.0.x as i32;
-    let y1 = rect_diagonale.0.y as i32;
-    let x2 = rect_diagonale.1.x as i32;
-    let y2 = rect_diagonale.1.y as i32;
+fn crosses_rect_inside(Rect { p1, p2 }: &Rect, line: &Edge) -> bool {
+    let rmin_x = p1.x.min(p2.x);
+    let rmax_x = p1.x.max(p2.x);
+    let rmin_y = p1.y.min(p2.y);
+    let rmax_y = p1.y.max(p2.y);
 
-    let x3 = line2.0.x as i32;
-    let y3 = line2.0.y as i32;
-    let x4 = line2.1.x as i32;
-    let y4 = line2.1.y as i32;
-
-    let deonimnator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-    if deonimnator == 0 {
-        return false;
+    match *line {
+        Edge::Vertical { x, y_min, y_max } => {
+            // Edge is between the left and right walls
+            // AND the vertical span of the edge overlaps the vertical interior of the rect
+            (rmin_x < x && x < rmax_x) && (y_min < rmax_y && y_max > rmin_y)
+        }
+        Edge::Horizontal { y, x_min, x_max } => {
+            // Edge is between the top and bottom walls
+            // AND the horizontal span of the edge overlaps the horizontal interior of the rect
+            (rmin_y < y && y < rmax_y) && (x_min < rmax_x && x_max > rmin_x)
+        }
     }
-
-    let px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) as f32
-        / deonimnator as f32;
-    let py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) as f32
-        / deonimnator as f32;
-
-    let xmin = rect_diagonale.0.x.min(rect_diagonale.1.x) as f32;
-    let ymin = rect_diagonale.0.y.min(rect_diagonale.1.y) as f32;
-    let xmax = rect_diagonale.0.x.max(rect_diagonale.1.x) as f32;
-    let ymax = rect_diagonale.0.y.max(rect_diagonale.1.y) as f32;
-
-    px > xmin && px < xmax && py > ymin && py < ymax
 }
 
 #[cfg(test)]
@@ -154,9 +189,9 @@ mod tests {
         assert_eq!(part2(&input), 24);
     }
 
-    //#[test]
+    #[test]
     fn input_part2() {
         let input = parse_input(INPUT);
-        assert_eq!(part2(&input), todo!());
+        assert_eq!(part2(&input), 1479665889);
     }
 }
