8ffe26586cffbfcb07dd6eac52af1d4ed8fd8302
Tobias Tangemann <tobias@tangemann.org>
2025-12-26 00:11:22 +0100

    Cleanup

--- 2025/src/day09.rs
+++ 2025/src/day09.rs
@@ -30,13 +30,24 @@ enum Edge {
     Horizontal { y: Number, x_min: Number, x_max: Number },
 }
 
-impl From<(Vec2, Vec2)> for Edge {
+impl Edge {
     #[rustfmt::skip]
-    fn from((p1, p2): (Vec2, Vec2)) -> Self {
+    fn from(p1: &Vec2, p2: &Vec2) -> Self {
         if p1.y == p2.y {
             Edge::Horizontal { y: p1.y, x_min: p1.x.min(p2.x), x_max: p1.x.max(p2.x) }
         } else {
             Edge::Vertical { x: p1.x, y_min: p1.y.min(p2.y), y_max: p1.y.max(p2.y) }
+        }
+    }
+
+    fn intersects(&self, other: &Edge) -> bool {
+        match (self, other) {
+            (Edge::Vertical { x, y_min, y_max }, Edge::Horizontal { y, x_min, x_max })
+            | (Edge::Horizontal { y, x_min, x_max }, Edge::Vertical { x, y_min, y_max }) => {
+                (x_min <= x && x <= x_max) && (y_min <= y && y <= y_max)
+            }
+            (Edge::Horizontal { .. }, Edge::Horizontal { .. })
+            | (Edge::Vertical { .. }, Edge::Vertical { .. }) => false,
         }
     }
 }
@@ -47,19 +58,29 @@ struct Rect {
     p2: Vec2,
 }
 
-impl std::fmt::Display for Rect {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "Rect [{{ y:{}, x:{} }}, {{ y:{}, x:{} }}]",
-            self.p1.y, self.p1.x, self.p2.y, self.p2.x
-        )
-    }
-}
-
 impl Rect {
     fn size(&self) -> Number {
         (self.p1.x.abs_diff(self.p2.x) + 1) * (self.p1.y.abs_diff(self.p2.y) + 1)
+    }
+
+    fn crosses_rect_inside(&self, line: &Edge) -> bool {
+        let rmin_x = self.p1.x.min(self.p2.x);
+        let rmax_x = self.p1.x.max(self.p2.x);
+        let rmin_y = self.p1.y.min(self.p2.y);
+        let rmax_y = self.p1.y.max(self.p2.y);
+
+        match *line {
+            Edge::Vertical { x, y_min, y_max } => {
+                // Edge is between the left and right walls
+                // AND the vertical span of the edge overlaps the vertical interior of the rect
+                (rmin_x < x && x < rmax_x) && (y_min < rmax_y && y_max > rmin_y)
+            }
+            Edge::Horizontal { y, x_min, x_max } => {
+                // Edge is between the top and bottom walls
+                // AND the horizontal span of the edge overlaps the horizontal interior of the rect
+                (rmin_y < y && y < rmax_y) && (x_min < rmax_x && x_max > rmin_x)
+            }
+        }
     }
 }
 
@@ -76,28 +97,25 @@ pub fn part1(input: &ParseResult) -> usize {
 #[aoc(day9, part2)]
 pub fn part2(coordinates: &ParseResult) -> usize {
     let mut larges_size = 0;
-    // let mut top_size = None;
 
     let edges = coordinates
         .iter()
         .chain(&[coordinates[0]]) // Append the first to the end to close polygon
         .tuple_windows()
-        .fold(vec![], |mut edges, (p1, p2)| {
-            edges.push((*p1, *p2).into());
-            edges
-        });
+        .map(|(p1, p2)| Edge::from(p1, p2))
+        .collect::<Vec<Edge>>();
 
     // Go over all possible rects
     'outer: for (&p1, &p2) in coordinates.iter().tuple_combinations() {
         let rect = Rect { p1, p2 };
         let size = rect.size();
-        if size < larges_size {
+        if size <= larges_size {
             continue;
         }
 
+        // Reject if polgon edge reaches inside the rect
         for polygon_edge in &edges {
-            if crosses_rect_inside(&rect, polygon_edge) {
-                // println!("Rejected {rect}: {size} because of edge:{polygon_edge:?}");
+            if rect.crosses_rect_inside(polygon_edge) {
                 continue 'outer;
             }
         }
@@ -112,56 +130,13 @@ pub fn part2(coordinates: &ParseResult) -> usize {
             y_max: check_pos_inside_y,
         };
 
-        let mut intersection_count = 0;
-
-        for polygon_edge in &edges {
-            if intersects_edge(&ray, polygon_edge) {
-                intersection_count += 1;
-            }
+        // Odd intersection count means inside
+        if edges.iter().filter(|edge| edge.intersects(&ray)).count() % 2 != 0 {
+            larges_size = size;
         }
-
-        if intersection_count % 2 == 0 {
-            // println!("Rejected {rect}: {size} intersection_count:{intersection_count}");
-            continue 'outer;
-        }
-
-        larges_size = size;
-        // top_size = Some((p1, p2));
-        // println!("----- Found {rect}: {size} intersection_count:{intersection_count}");
     }
 
     larges_size
-}
-
-fn intersects_edge(line1: &Edge, line2: &Edge) -> bool {
-    match (line1, line2) {
-        (Edge::Vertical { x, y_min, y_max }, Edge::Horizontal { y, x_min, x_max })
-        | (Edge::Horizontal { y, x_min, x_max }, Edge::Vertical { x, y_min, y_max }) => {
-            (x_min <= x && x <= x_max) && (y_min <= y && y <= y_max)
-        }
-        (Edge::Horizontal { .. }, Edge::Horizontal { .. })
-        | (Edge::Vertical { .. }, Edge::Vertical { .. }) => false,
-    }
-}
-
-fn crosses_rect_inside(Rect { p1, p2 }: &Rect, line: &Edge) -> bool {
-    let rmin_x = p1.x.min(p2.x);
-    let rmax_x = p1.x.max(p2.x);
-    let rmin_y = p1.y.min(p2.y);
-    let rmax_y = p1.y.max(p2.y);
-
-    match *line {
-        Edge::Vertical { x, y_min, y_max } => {
-            // Edge is between the left and right walls
-            // AND the vertical span of the edge overlaps the vertical interior of the rect
-            (rmin_x < x && x < rmax_x) && (y_min < rmax_y && y_max > rmin_y)
-        }
-        Edge::Horizontal { y, x_min, x_max } => {
-            // Edge is between the top and bottom walls
-            // AND the horizontal span of the edge overlaps the horizontal interior of the rect
-            (rmin_y < y && y < rmax_y) && (x_min < rmax_x && x_max > rmin_x)
-        }
-    }
 }
 
 #[cfg(test)]
